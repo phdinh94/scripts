c9485b6f268063ea347dba2fd77208406a38305d
Phuc Dinh <phdinh94@gmail.com>
2021-02-23 16:37:15 -0500

    ns-3 installatin

---
+++ build-prerequisite.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+ apt-get -y install g++ python3 
+ apt-get -y install g++ python3 python3-dev pkg-config sqlite3
+ apt-get -y install python3-setuptools git
+ apt-get -y install qt5-default mercurial
+ apt-get -y install gir1.2-goocanvas-2.0 python-gi python-gi-cairo python-pygraphviz python3-gi python3-gi-cairo python3-pygraphviz gir1.2-gtk-3.0 ipython ipython3  
+ apt-get -y install openmpi-bin openmpi-common openmpi-doc libopenmpi-dev
+ apt-get -y install autoconf cvs bzr unrar
+ apt-get -y install gdb valgrind 
+ apt-get -y install uncrustify
+ apt-get -y install doxygen graphviz imagemagick
+ apt-get -y install texlive texlive-extra-utils texlive-latex-extra texlive-font-utils dvipng latexmk
+ apt-get -y install python3-sphinx dia 
+ apt-get -y install gsl-bin libgsl-dev libgsl23 libgslcblas0
+ apt-get -y install tcpdump
+ apt-get -y install sqlite sqlite3 libsqlite3-dev
+ apt-get -y install libxml2 libxml2-dev
+ apt-get -y install cmake libc6-dev libc6-dev-i386 libclang-6.0-dev llvm-6.0-dev automake pip
+ python3 -y -m pip install --user cxxfilt
+ apt-get -y install libgtk-3-dev
+ apt-get -y install vtun lxc uml-utilities
+ apt-get -y install libxml2 libxml2-dev
+
+
---
+++ ns3-installation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+ git clone https://gitlab.com/nsnam/bake
+ export BAKE_HOME=`pwd`/bake
+ export PATH=$PATH:$BAKE_HOME
+ export PYTHONPATH=$PYTHONPATH:$BAKE_HOME
+ bake.py check
+ bake.py configure -e ns-3.29
+ bake.py download
+ bake.py build
+
+
