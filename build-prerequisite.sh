2c626b5d6d8fb752f36538cd2e65ad98c0a24cf7
Phuc Dinh <phdinh94@gmail.com>
2021-02-26 23:14:31 -0500

    fixed some error packages

--- build-prerequisite.sh
+++ build-prerequisite.sh
@@ -1,18 +1,33 @@
-#!/bin/bash 
+#!/usr/bin/bash 
 
- apt-get -y install g++ python3 
- apt-get -y install g++ python3 python3-dev pkg-config sqlite3
+ apt-get -y install g++  
+ apt-get -y install python3 python3-dev 
+ apt-get -y install pkg-config
+ apt-get -y install sqlite3
  apt-get -y install python3-setuptools git
- apt-get -y install qt5-default mercurial
- apt-get -y install gir1.2-goocanvas-2.0 python-gi python-gi-cairo python-pygraphviz python3-gi python3-gi-cairo python3-pygraphviz gir1.2-gtk-3.0 ipython ipython3  
- apt-get -y install openmpi-bin openmpi-common openmpi-doc libopenmpi-dev
+ apt-get -y install qt5-default
+ apt-get -y install mercurial
+ apt-get -y install gir1.2-goocanvas-2.0
+ apt-get -y install python3-gi
+ apt-get -y install python3-gi-cairo
+ apt-get -y install python3-pygraphviz
+ apt-get -y install gir1.2-gtk-3.0 
+ apt-get -y install ipython 
+ apt-get -y install ipython3  
+ apt-get -y install openmpi-bin 
+ apt-get -y install openmpi-common 
+ apt-get -y install openmpi-doc 
+ apt-get -y install libopenmpi-dev
  apt-get -y install autoconf cvs bzr unrar
  apt-get -y install gdb valgrind 
  apt-get -y install uncrustify
  apt-get -y install doxygen graphviz imagemagick
  apt-get -y install texlive texlive-extra-utils texlive-latex-extra texlive-font-utils dvipng latexmk
  apt-get -y install python3-sphinx dia 
- apt-get -y install gsl-bin libgsl-dev libgsl23 libgslcblas0
+ apt-get -y install gsl-bin 
+ apt-get -y install libgsl-dev 
+ apt-get -y install libgsl23 
+ apt-get -y install libgslcblas0
  apt-get -y install tcpdump
  apt-get -y install sqlite sqlite3 libsqlite3-dev
  apt-get -y install libxml2 libxml2-dev
--- ns3-installation.sh
+++ ns3-installation.sh
@@ -4,6 +4,7 @@
  export BAKE_HOME=`pwd`/bake
  export PATH=$PATH:$BAKE_HOME
  export PYTHONPATH=$PYTHONPATH:$BAKE_HOME
+ cd ~/bake
  bake.py check
  bake.py configure -e ns-3.29
  bake.py download
