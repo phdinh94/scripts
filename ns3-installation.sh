e59ac1fcf6f79495f3f1a02edeb33660c7d36452
Phuc Dinh <phdinh94@gmail.com>
2021-04-03 21:37:42 -0400

    moving ns-3 folder to home

--- ns3-installation.sh
+++ ns3-installation.sh
@@ -1,13 +1,15 @@
 #!/bin/bash 
-
+ echo "Enter the ns-3 version you want to install (i.e, ns-3.29)"
+ read version
  git clone https://gitlab.com/nsnam/bake
  export BAKE_HOME=`pwd`/bake
  export PATH=$PATH:$BAKE_HOME
  export PYTHONPATH=$PYTHONPATH:$BAKE_HOME
  cd ~/bake
  bake.py check
- bake.py configure -e ns-3.29
- bake.py download
- bake.py build
-
+ bake.py configure -e $version
+ bake.py show
+ bake.py deploy 
+ mv -r BAKE_HOME/source/ ~
+ 
 
